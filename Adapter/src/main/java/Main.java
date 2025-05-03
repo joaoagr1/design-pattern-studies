b9c8e0fd87670f68ffe65f79f8ff9f513c876eec
joaoagr1 <joaorolo.dev@gmail.com>
2025-05-03 16:21:55 -0300

    feat: adicionando factory method teoria e implementação.

--- Adapter/src/main/java/Main.java
+++ Adapter/src/main/java/Main.java
@@ -1,9 +1,43 @@
-public class Main {
+interface Transporte {
+    void entregar();
+}
 
-    public static void main(String[] args) {
-
-
-
+class Caminhao implements Transporte {
+    public void entregar() {
+        System.out.println("Entrega feita por caminhão.");
     }
+}
 
+class Navio implements Transporte {
+    public void entregar() {
+        System.out.println("Entrega feita por navio.");
+    }
+}
+
+abstract class Logistica {
+    public abstract Transporte criarTransporte();
+}
+
+class LogisticaTerrestre extends Logistica {
+    public Transporte criarTransporte() {
+        return new Caminhao();
+    }
+}
+
+class LogisticaMaritima extends Logistica {
+    public Transporte criarTransporte() {
+        return new Navio();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Logistica logistica1 = new LogisticaTerrestre();
+        Transporte transporte1 = logistica1.criarTransporte();
+        transporte1.entregar();
+
+        Logistica logistica2 = new LogisticaMaritima();
+        Transporte transporte2 = logistica2.criarTransporte();
+        transporte2.entregar();
+    }
 }
